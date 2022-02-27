318f81fc510bf4951355f507ba0c112a32d768a4
Adrian Mummey <amummey@gmail.com>
2022-02-27 17:56:40 -0300

    More fixes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # endala-web
+
 Frontend website for Endala NFT project
+
+## Directions
+
+Requires browserify and yarn
+`npm i -g browserify yarn`
+Then build and launch a server
+`yarn run build`
--- src/connectKeplr.js
+++ src/connectKeplr.js
@@ -1,4 +1,5 @@
-const cosmwasm = require("cosmwasm");
+import { SigningCosmWasmClient } from "cosmwasm";
+import connectIfNotExists from "./stargazeTestnet";
 
 // console.log(cosmwasm);
 
@@ -25,7 +26,7 @@ const setupWebKeplr = async () => {
   const offlineSigner = await window.getOfflineSignerAuto(config.chainId);
 
   // Init SigningCosmWasmClient client
-  const signingClient = await cosmwasm.SigningCosmWasmClient.connectWithSigner(
+  const signingClient = await SigningCosmWasmClient.connectWithSigner(
     config.rpcEndpoint,
     offlineSigner,
     {
@@ -37,74 +38,9 @@ const setupWebKeplr = async () => {
   return signingClient;
 };
 
-const defaultBech32Config = (
-  mainPrefix,
-  validatorPrefix = "val",
-  consensusPrefix = "cons",
-  publicPrefix = "pub",
-  operatorPrefix = "oper"
-) => {
-  return {
-    bech32PrefixAccAddr: mainPrefix,
-    bech32PrefixAccPub: mainPrefix + publicPrefix,
-    bech32PrefixValAddr: mainPrefix + validatorPrefix + operatorPrefix,
-    bech32PrefixValPub:
-      mainPrefix + validatorPrefix + operatorPrefix + publicPrefix,
-    bech32PrefixConsAddr: mainPrefix + validatorPrefix + consensusPrefix,
-    bech32PrefixConsPub:
-      mainPrefix + validatorPrefix + consensusPrefix + publicPrefix,
-  };
-};
-
 async function main() {
   // await window.keplr.enable(config.chainId);
-  console.log(
-    window.keplr.experimentalSuggestChain({
-      chainId: "big-bang-1",
-      rpc: "https://rpc.big-bang-1.stargaze-apis.com/",
-      rest: "https://rest.big-bang-1.stargaze-apis.com/",
-      chainName: "Stargaze Test",
-      bech32Config: defaultBech32Config("stars"),
-      bip44: {
-        coinType: 118,
-      },
-      stakeCurrency: {
-        coinDenom: "STARS",
-        coinMinimalDenom: "ustars",
-        coinDecimals: 6,
-        coinGeckoId: "stargaze",
-        coinImageUrl: "https://dhj8dql1kzq2v.cloudfront.net/white/stargaze.png",
-      },
-      nativeCurrency: "STARS",
-      currencies: [
-        {
-          coinDenom: "STARS",
-          coinMinimalDenom: "ustars",
-          coinDecimals: 6,
-          coinGeckoId: "stargaze",
-          coinImageUrl:
-            "https://dhj8dql1kzq2v.cloudfront.net/white/stargaze.png",
-        },
-      ],
-      feeCurrencies: [
-        {
-          coinDenom: "STARS",
-          coinMinimalDenom: "ustars",
-          coinDecimals: 6,
-          coinGeckoId: "stargaze",
-          coinImageUrl:
-            "https://dhj8dql1kzq2v.cloudfront.net/white/stargaze.png",
-        },
-      ],
-      features: ["stargate", "no-legacy-stdTx", "ibc-transfer", "ibc-go"],
-      chainSymbolImageUrl:
-        "https://dhj8dql1kzq2v.cloudfront.net/white/stargaze.png",
-      txExplorer: {
-        name: "TestScan",
-        txUrl: "http://38.242.223.192/big-bang-1/tx/{txHash}",
-      },
-    })
-  );
+  connectIfNotExists();
 
   const client = await setupWebKeplr(config);
   console.log(client);
